e6c50d67cda2be95bd9cbafa58190d1371a72575
Brian Walshe <walshe.brian@gmail.com>
2025-04-10 17:36:50 +0100

    added attention mask

--- src/customhf/model.py
+++ src/customhf/model.py
@@ -1,7 +1,11 @@
-from torch import nn
+from torch import nn, Tensor
 
-from transformers import PretrainedConfig, PreTrainedModel, GenerationMixin
-from transformers.models.auto.modeling_auto import MODEL_FOR_CAUSAL_LM_MAPPING
+from transformers import (
+    PretrainedConfig,
+    PreTrainedModel,
+    GenerationMixin,
+    AutoModelForCausalLM,
+    AutoConfig)
 from transformers.modeling_outputs import CausalLMOutput
 
 
@@ -15,22 +19,35 @@ class BigramLanguageModel(PreTrainedModel, GenerationMixin):
     def __init__(self, config):
         super().__init__(config)
         self.vocab_size = config.vocab_size
+        self.pad_token_id = -100
         self.token_embedding_table = nn.Embedding(
-            self.vocab_size, self.vocab_size)
+            self.vocab_size, self.vocab_size, padding_idx=self.pad_token_id)
         self.loss_type = "ForCausalLM"
 
-    def forward(self, input_ids, labels=None, **kwargs):
+    def forward(self,
+                input_ids: Tensor,
+                attention_mask: Tensor = None,
+                labels: Tensor = None,
+                **kwargs) -> CausalLMOutput:
         """Args:
             idx: (B,T) tensor of integers
             targets: (B,T) tensor of integers
         """
+        input_ids = self._apply_mask(input_ids, attention_mask)
         logits = self.token_embedding_table(input_ids)
 
         loss = None
         if labels is not None:
+            labels = self._apply_mask(labels, attention_mask)
             loss = self.loss_function(
-                logits, labels, vocab_size=self.vocab_size)
+                logits, labels, vocab_size=self.vocab_size, ignore_index=self.pad_token_id)
         return CausalLMOutput(loss=loss, logits=logits)
 
+    def _apply_mask(self, values: Tensor, mask: Tensor) -> Tensor:
+        if mask.all():
+            return values
+        return values.masked_fill(mask.bool(), self.pad_token_id)
 
-MODEL_FOR_CAUSAL_LM_MAPPING.register("bigram-language", BigramLanguageModel)
+
+AutoConfig.register("bigram-language", BigramLanguageModelConfig)
+AutoModelForCausalLM.register(BigramLanguageModelConfig, BigramLanguageModel)
