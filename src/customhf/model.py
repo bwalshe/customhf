fixed bug in attention mask

src/customhf/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn, Tensor
 
 from transformers import (
@@ -21,7 +22,7 @@ class BigramLanguageModel(PreTrainedModel, GenerationMixin):
         self.vocab_size = config.vocab_size
         self.pad_token_id = -100
         self.token_embedding_table = nn.Embedding(
-            self.vocab_size, self.vocab_size, padding_idx=self.pad_token_id)
+            self.vocab_size, self.vocab_size)
         self.loss_type = "ForCausalLM"
 
     def forward(self,
@@ -33,7 +34,6 @@ class BigramLanguageModel(PreTrainedModel, GenerationMixin):
             idx: (B,T) tensor of integers
             targets: (B,T) tensor of integers
         """
-        input_ids = self._apply_mask(input_ids, attention_mask)
         logits = self.token_embedding_table(input_ids)
 
         loss = None
@@ -44,9 +44,11 @@ class BigramLanguageModel(PreTrainedModel, GenerationMixin):
         return CausalLMOutput(loss=loss, logits=logits)
 
     def _apply_mask(self, values: Tensor, mask: Tensor) -> Tensor:
-        if mask.all():
+        if mask is None or mask.all():
             return values
+        mask = torch.ones_like(mask) - mask
         return values.masked_fill(mask.bool(), self.pad_token_id)
+
 
 def register_bigram_language_model():
     AutoConfig.register("bigram-language", BigramLanguageModelConfig)

test/test_model.py
@@ -16,7 +16,7 @@ def test_loss_fn():
     input_ids = torch.randint(vocab_size, (batch_size, input_len))
     targets = input_ids.clone()
 
-    result = model(input_ids=input_ids, attention_mask=None, labels=targets)
+    result = model(input_ids=input_ids, labels=targets)
     loss = result["loss"]
     logits = result["logits"].float()
 
@@ -31,3 +31,25 @@ def test_loss_fn():
         logits, targets, ignore_index=ignore_index)
 
     assert loss == karpathy_loss
+
+
+def test_attention_mask():
+    vocab_size = 2
+    examples = 10
+    config = BigramLanguageModelConfig(vocab_size=vocab_size)
+    model = BigramLanguageModel(config)
+    with torch.no_grad():
+        model.token_embedding_table.weight = torch.nn.Parameter(
+            torch.eye(vocab_size))
+
+    input_ids = torch.ones(1, examples, dtype=torch.long)
+    labels = torch.cat(
+        (torch.ones(1, examples // 2, dtype=torch.long),
+         torch.zeros(1, examples // 2, dtype=torch.long)),
+        1)
+    mask = labels.clone()
+    result_no_mask =  model(input_ids=input_ids, labels=labels)
+    result_short = model(input_ids=input_ids[:, :examples // 2], labels=labels[:, :examples // 2])
+    result_masked = model(input_ids=input_ids, attention_mask=mask, labels=labels)
+    assert result_short["loss"] < result_no_mask["loss"]
+    assert result_short["loss"] == result_masked["loss"]